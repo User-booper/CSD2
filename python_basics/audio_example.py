3910cf71ee7faafce2cb852634779181c7c3fe85
Lau Breukhoven <breukhovenlaura@gmail.com>
2025-09-17 11:10:30 +0200

    opdrachten les2

--- python_basics/audio_example.py
+++ python_basics/audio_example.py
@@ -10,6 +10,8 @@ sample = pygame.mixer.Sound('./plop.wav')
 play_amount = int(input("How many times would you like to hear the sample ?: "))
 # aantal keer dat de sample wordt afgespeeld.
 
+
+#audio afspelen dmv een forloop
 for x in range(0, play_amount):
     sample.play()
     # time.sleep(sample.get_length())
@@ -17,4 +19,18 @@ for x in range(0, play_amount):
 
 # play the sample and wait till sound is done playing / 0.5seconds before exiting
 
+#audio afspelen dmv een while loop
+
+playing = True
+count = 0
+
+while(playing):
+    sample.play
+    count =+ 1
+    time.sleep(0.5)
+
+    if count > play_amount:
+        playing = False
+
+    
 
