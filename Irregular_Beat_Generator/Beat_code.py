5aa14f94b2a83b7391dd5ee3447f34216e954da1
Lau Breukhoven <breukhovenlaura@gmail.com>
2025-09-30 21:31:35 +0200

    Updated code to include LCG

--- Irregular_Beat_Generator/Beat_code.py
+++ Irregular_Beat_Generator/Beat_code.py
@@ -1,52 +1,74 @@
 #Irregular_Beat_Generator
 import math
 import pygame as pg
-#Math library voor het berekenen van de interpolatiefunties etc.
-#Pygame library voor het afspelen van geluid
+import matplotlib.pyplot as plt
 
-#TO DO: Met PRNG Waardes aan punten t = 1, 2, 3, ...9 toeschrijven.
-# PRNG: LCG
-#Variabelen: 
-#a = vermenigvuldigingsfactor = 1664525
-#b = increment = 1013904223
-#m = modulus = 2**31 -1
+pg.init()
+#Math library to caculate interpolation functions
+#Pygame library for playing sound
 
-#LCG: vorm functie:
-# y(n+1) = a * y(n) + b % m
+
+# TO DO: PRNG: LCG
+
 # LCG functie; takes in amount of numbers that need to be generated and a seed.
 seed = int(input( "Enter a random number: "))
 
-def LCG(amount_of_numbers, seed):
+def LCG(amount_of_numbers, start_int):
   a = 1664525
   b = 10013904223
   m = 2**31 - 1 
+  lcg_generated_numbers = []
   
-  for i in range(0, amount_of_numbers):
-    num = (a * seed + b) % m #berekent num0 en voegt deze toe aan een lijst
-    lcg_numbers = []
-    lcg_number.append(num)
+  for number in range(amount_of_numbers):
+    generated_number = (a * start_int + b) % m #calculates number[0] and ads it to the end of a list.
+    rounded_number = round(generated_number / m, 2) #scale the number between 0 -1, round to two decimals.
+    lcg_generated_numbers.append(rounded_number) # add rounded number to list.
+
+    start_int = generated_number #the generated number is used as the start_int for the next iteration.
+
+  return lcg_generated_numbers 
+
+lcg_numbers = LCG(10, seed)
+mu = LCG(9, seed + 10) #where a t is located between two set intervals
+print(lcg_numbers)
+
+#TO DO: coupling the generated numbers to values at set intervals (time + value pairs).
+
+set_intervals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Dictionary with computed data
+generated_graph_data = {
+  "intervals": set_intervals,
+  "values": lcg_numbers,
+  "mu": mu
+}
 
 
-    seed = num #de seed voor de volgende iteratie wordt het vorige getal
-    
-   
-
-  return lcg_numbers
-
-
-#TO DO: Lineaire Interpolatie tussen punten op vaste intervallen
+#print(generated_graph_data)
+#TO DO: Lineaire Interpolatie between set intervals on grid.
 #Linear Interpolation
 def linearInterpolate(y1, y2, mu):
   return (y2 - y1) * mu + y1
 
 
-#TO DO: Afronding van lineaire grafiek.
+#TO DO: Rounding of linear function
 #Cosine Step Interpolation
 def cosineInterpolate(y1, y2, mu):
   angle = mu * math.pi 
-  mu2 = (1.0f - cos(angle)) * 0.5f # get new x-value based on cosine function
+  mu2 = (1.0 - math.cos(angle)) * 0.5 # get new x-value based on cosine function
   return linearInterpolate(y1, y2, mu2)
 
 
 
-#TO DO: Manier bedenken om waardes toe te schrijven aan een grid.
+#TO DO: Think of way to add value to gtid
+#Plot generated graph
+time = set_intervals
+chance = lcg_numbers
+
+plt.plot(time, chance, mu)
+
+plt.xlabel('Time')
+plt.ylabel("Chance")
+plt.title("Plot")
+
+plt.show()
